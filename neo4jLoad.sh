61faa167cf4b0dd316c5960825d3654367c0a727
Timothy Whittaker <timsetsfire@gmail.com>
2017-10-06 23:58:11 -0400

    Update neo4jLoad.sh
    Add note concerning file extension on neo4j-import.

--- neo4jLoad.sh
+++ neo4jLoad.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+## in windows, you will likely need to add file extension to neo4j-import
+## i.e., neo4j-import.bat
 $HOME/Desktop/neo4j/bin/neo4j-import --into bikegraph.db \
                  --nodes:Station ./data/processed/vertices.csv \
                  --relationships:TRANSITION ./data/processed/edges-markov.csv  \
